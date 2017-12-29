3372fa25f81b7129676b29ee563cc640d9cc6bf3
Anonymous Applicant <anonymous@example.com>
2017-12-29 13:40:04 -0700

    API request path created from form data

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -1,6 +1,21 @@
 (ns my-exercise.search)
 
+(defn sanitize-params [params]
+  (def clean-city (clojure.string/lower-case (clojure.string/replace (get params "city") #" " "_")))
+  (def clean-state (clojure.string/lower-case (get params "state")))
+  {:street (get params "street")
+    :street-2 (get params "street-2")
+    :city clean-city
+    :state clean-state
+    :zip (get params "zip")})
+
+(defn create-ocd-ids [location]
+  (def ocd-state (str "ocd-division/country:us/state:" (get location :state)))
+  (def ocd-city (str ocd-state "/place:" (get location :city)))
+  (str ocd-state "," ocd-city))
+
 (defn search [request]
-  (def form-data (get request :form-params))
-  (println form-data)
+  (def location (sanitize-params (get request :form-params)))
+  (def ocd-ids (create-ocd-ids location))
+  (println ocd-ids)
   "testing")
